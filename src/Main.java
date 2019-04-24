4b0b637d1612e5bf11d702c2eda8c8766ca902b7
YuriyKlots <sprklyots@gmail.com>
2019-04-25 00:30:49 +0300

    Lection 5

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Robot robot = new Robot();
         robot.work();
+
+        Robot coffeRobot = new CoffeRobot();
+        coffeRobot.work();
+
+        Robot robotDancer = new RobotDancer();
+        robotDancer.work();
+
+        Robot robotCoocker = new RobotCoocker();
+        robotCoocker.work();
     }
 }
